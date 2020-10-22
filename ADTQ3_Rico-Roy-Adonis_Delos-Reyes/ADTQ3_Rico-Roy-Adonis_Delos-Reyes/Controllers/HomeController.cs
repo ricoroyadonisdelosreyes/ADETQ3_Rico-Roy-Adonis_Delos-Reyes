174fb2c47373275dfe17aeec413ed3691ad4c49f
RICO ROY ADONIS DELOS REYES <1244-19@itmlyceumalabang.onmicrosoft.com>
2020-10-21 22:18:04 -1200

    Delos Reyes, Rico Roy Adonis

---
+++ ADTQ3_Rico-Roy-Adonis_Delos-Reyes/ADTQ3_Rico-Roy-Adonis_Delos-Reyes/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADTQ3_Rico_Roy_Adonis_Delos_Reyes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good AM" : "Good PM";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ADTQ3_Rico-Roy-Adonis_Delos-Reyes/ADTQ3_Rico-Roy-Adonis_Delos-Reyes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADTQ3_Rico_Roy_Adonis_Delos_Reyes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
